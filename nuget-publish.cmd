0e8f736d8397b3cefe43a4e5a1ac1366dc4c1c9c
Andriy Kozachuk <dev.oyster@gmail.com>
2013-02-12 23:08:49 +0200

    Version 1.0.0.2 release description changes

--- nuget-publish.cmd
+++ nuget-publish.cmd
@@ -1,4 +1,4 @@
 @cd IntXLib
-..\.nuget\nuget.exe pack IntXLib.csproj -Version 1.0.0.1 -Prop Configuration=Release -OutputDirectory bin
-..\.nuget\nuget.exe push bin\IntX.1.0.0.1.nupkg -ApiKey
+..\.nuget\nuget.exe pack IntXLib.csproj -Version 1.0.0.2 -Prop Configuration=Release -OutputDirectory bin
+..\.nuget\nuget.exe push bin\IntX.1.0.0.2.nupkg -ApiKey
 @pause
